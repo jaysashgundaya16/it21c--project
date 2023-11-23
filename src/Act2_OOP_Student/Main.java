960f9b357e4370a0fcbcbd3ee4e628071969bf21
jaysashgundaya16 <jaysashgundaya17@gmail.com>
2023-11-23 16:33:39 +0800

    added my initial file which is Main and Stdent Class

---
+++ src/Act2_OOP_Student/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+    
+        Student student = new Student("Jaysash I. Gundaya", "IT21C", "When life hands you lemons, make lemonade");
+        
+        System.out.println("Name: " + student.getName());
+        System.out.println("Section: " + student.getSection());
+        System.out.println("Motto: " + student.getMotto());
+    }
+}
