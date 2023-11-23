ea9aa9ebb81b95e6351a2e1a260cc9f70488c698
jaysashgundaya16 <jaysashgundaya17@gmail.com>
2023-11-23 16:38:59 +0800

    added initial files class1 and class2

---
+++ src/Act4_OOP_TwoClasses/Class1.java
@@ -0,0 +1,13 @@
+
+package TwoClasses;
+
+
+public class Class1 {
+    
+    public static void main (String []args){
+        
+      System.out.println("Hello to my dark side.");
+      System.out.println("Someday.");
+      System.out.println("Everything will be alright.");
+    }
+}
