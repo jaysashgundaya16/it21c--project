2e3bf35058c7d5f4afe04f1578ba7c8827d05848
jaysashgundaya16 <jaysashgundaya17@gmail.com>
2023-11-26 09:31:43 +0800

    added my Cat class

--- src/Act10_OOP_Laboratory Exercise no. 2/src/tulips.java
+++
@@ -1,9 +0,0 @@
-package laboratoryexercisefinalsno2;
-
-public class Tulips extends Flowers{
-    
-    @Override
-    public void Color(){
-        System.out.println("is beautiful and elegant.");
-    }
-}
