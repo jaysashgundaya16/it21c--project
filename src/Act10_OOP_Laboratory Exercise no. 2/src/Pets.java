d9df9c6fe00f202a0b50cd7b2904fd385142cd0f
jaysashgundaya16 <jaysashgundaya17@gmail.com>
2023-11-26 09:32:08 +0800

    added my Cat class

---
+++ src/Act10_OOP_Laboratory Exercise no. 2/src/Pets.java
@@ -0,0 +1,12 @@
+package laboratoryexercisefinalsno2;
+
+abstract class Pets {
+    public abstract void Color();
+    
+    public void Dog(){
+        System.out.print("[Kobe ");
+    }
+    public void Cat(){
+        System.out.print("{Mingbrown ");
+    }
+}
